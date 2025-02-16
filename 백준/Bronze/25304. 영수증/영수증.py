ea21894efb9fd5ab4204b1167ed842090672c153
BunnyByee <kimhime714@naver.com>
2025-02-16 19:38:15 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+result = 0
+
+x = int(input())
+n = int(input())
+
+for i in range(n) :
+    a, b = map(int, input().split())
+    result = result + a*b
+
+if x == result :
+    print('Yes')
+else :
+    print('No')
