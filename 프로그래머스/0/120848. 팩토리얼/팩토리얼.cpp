1d0abdebceaba7332c9dba68aa173cbfcfd700b2
BunnyByee <kimhime714@naver.com>
2025-03-07 09:54:46 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
@@ -5,17 +5,14 @@ using namespace std;
 
 int factorial(int n){
     if (n <= 1) return 1;
-    
     return n * factorial(n-1);
 }
 
 int solution(int n) {
-    int answer = 0;
-    int i = 1;
-    while(n >= factorial(i)){
-        answer = i;
+    int i = 0;
+    while(factorial(i) <= n){
         i++;
     }
-    return answer;
+    return i-1;
     
 }
