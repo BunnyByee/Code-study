7d321e8b751eb2ddcf2c7f2a8240150a451f4653
BunnyByee <kimhime714@naver.com>
2025-03-08 22:32:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.06 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 08일 22:02:38
+2025년 03월 08일 22:32:34
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -6,21 +6,25 @@ using namespace std;
 long long solution(int a, int b) {
     long long answer = 0;
     
-    // a와 b 사이에 속한 모든 정수를 더하기
-    // a < b -> a ++
-    if (a < b) {
-        for (int i = a; i <= b; i++){
-        answer += i;
-        }
-    }
-    // a = b -> return a
-    else if (a == b) return a;
-    // a < b -> a--
+    if (a == b) answer = a;
     else {
-        for (int i = a; i >= b; i--){
-        answer += i;
-        }
+        answer = (long long)(a+b) * (abs(a-b) + 1) / 2;
     }
-    
     return answer;
+    
+    // // a와 b 사이에 속한 모든 정수를 더하기
+    // // a < b -> a ++
+    // if (a < b) {
+    //     for (int i = a; i <= b; i++){
+    //     answer += i;
+    //     }
+    // }
+    // // a = b -> return a
+    // else if (a == b) return a;
+    // // a < b -> a--
+    // else {
+    //     for (int i = a; i >= b; i--){
+    //     answer += i;
+    //     }
+    // }
 }
