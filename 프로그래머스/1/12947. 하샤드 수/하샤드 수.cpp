b618145d7915aacc6c14956357bac256311173be
BunnyByee <kimhime714@naver.com>
2025-03-10 10:00:59 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -4,15 +4,26 @@
 using namespace std;
 
 bool solution(int x) {
-    string str = to_string(x);
+    int sum = 0;
+//  int num = x;
+    
+//  while(num > 0){
+//      sum += num % 10;
+//      num /= 10;
+//   }
+    
+//   return (x % sum == 0) ? true : false;
     
     // 문자열 개수만큼 자릿수의 값을 합하기
-    int i = 0;
-    int sum = 0;
+    string str = to_string(x);
     
-    for (int i = 0; i < str.length(); i++) {
-    sum += (str[i] - '0');
+    for (char c: str) {
+        sum += (c - '0');
     }
+    
+    // for (int i = 0; i < str.length(); i++) {
+    //     sum += (str[i] - '0');
+    // }
 
     bool answer = (x % sum == 0);
     return answer;
