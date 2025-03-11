e1ded1ed756b6ed09507edb871ac1a264ba12ffe
BunnyByee <kimhime714@naver.com>
2025-03-11 09:32:21 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.cpp
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -6,8 +6,8 @@ using namespace std;
 string solution(string rny_string) {
     string answer = "";
     string str = rny_string;
-    int size = str.length();
-    
+    // 문자열의 문자 c가 m이면 "rm" 넣기
+    // 그게 아니면 문자 c 넣기
     for (char c : str){
         if (c == 'm') answer += "rn";
         else answer += c;
