1ed5109a8e1e31fccbb42c6dc13373e1daf3d212
BunnyByee <kimhime714@naver.com>
2025-03-12 09:44:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 1.40 ms, Memory: 6.57 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> stk;
-    int i = 0;
     
+    int i = 0;
     // 변수 i = 0, i가 arr길이보다 작을때까지 반복
     while(i < arr.size()){
         // stk 비어있다면 arr[i] 추가하고 i++
@@ -15,16 +15,13 @@ vector<int> solution(vector<int> arr) {
             i++;
         }
         // 원소가 있다면 stk 마지막 원소 < arr[i] -> arr[i]를 뒤에 추가 i++
-        else {
-            if (stk.back() < arr[i]) {
+        else if (stk.back() < arr[i]) {
                 stk.push_back(arr[i]);
                 i++;
             }
              // -> stk 마지막 원소 >= arr[i] 이라면 stk 마지막 원소 제거.
-            else {
-                stk.pop_back();
-            }
-        }
+        else stk.pop_back();
+        
     }
     return stk;
 }
