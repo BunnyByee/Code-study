6f7b13acfb7b56c925aacd69040c1858a26c4128
BunnyByee <kimhime714@naver.com>
2025-03-13 18:05:17 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,8 +1,15 @@
 n = int(input())
 
-while(n != 1):
-    for i in range(2,n+1):
-        if (n % i == 0) :
-            print(i)
-            break
-    n //= i
+while(n%2 == 0):
+    print(2)
+    n //= 2
+
+i = 3
+while (i * i <= n):
+    while n % i == 0 :
+        print(i)
+        n //= i
+    i += 2
+
+if n > 1:
+    print(n)
