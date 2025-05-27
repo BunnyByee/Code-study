08730758ce308a4b62d90adfa39b5ea29908aeff
김희수 <khs.bunnybyee@gmail.com>
2025-05-27 18:02:10 +0900

    [Silver IV] Title: 대칭 차집합, Time: 236 ms, Memory: 82648 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -5,7 +5,6 @@ s1 = set(map(int,input().split()))
 s2 = set(map(int,input().split()))
 
 # 대칭 차집합의 원소의 개수 더하기
-num += len(s1 - s2)
-num += len(s2 - s1)
+num = len(s1 - s2) + len(s2 - s1)
 
 print(num)
