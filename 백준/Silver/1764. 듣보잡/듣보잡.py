f05b1be93f309e919f0191c2a84ac3c87b68366d
김희수 <khs.bunnybyee@gmail.com>
2025-05-27 17:50:49 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 43168 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,7 +1,11 @@
+import sys
+# 개행 추가됨
+input = sys.stdin.readline
+
 n, m = map(int,input().split())
 
-no_listen = set(input() for _ in range(n))
-no_look = set(input() for _ in range(m))
+no_listen = set(input().strip() for _ in range(n))
+no_look = set(input().strip() for _ in range(m))
 
 # 교집합
 result = sorted(no_listen & no_look)
