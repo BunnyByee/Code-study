2da13dc8d82da36f2aea061536310a01a221eb6d
김희수 <khs.bunnybyee@gmail.com>
2025-06-27 11:51:21 +0900

    [Silver III] Title: N과 M (3), Time: 1916 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+path = []
+
+def dfs(path) :
+    if len(path) == M:
+        print(*path)
+        return
+    
+    for i in range(1, N+1):
+        dfs(path + [i]) # 리스트에 i 추가
+
+dfs([])
