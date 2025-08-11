83dbda8f6bbdea309cceeba5ea97a2b061488b99
김희수 <khs.bunnybyee@gmail.com>
2025-08-11 22:41:24 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int T;
+	cin >> T;
+	
+	for (int i=0; i<T; i++){
+		string s;
+		cin >> s;
+		cout << s.front() << s.back() << '\n';
+	}
+}
