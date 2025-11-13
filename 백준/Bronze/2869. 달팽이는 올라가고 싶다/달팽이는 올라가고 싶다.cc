4b9e40fa435fd30343d4bc00e12a9607bafed06a
김희수 <khs.bunnybyee@gmail.com>
2025-11-13 17:43:08 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int A, B, V;
+	
+	cin >> A >> B >> V;
+	
+	int day = (V-A) / (A-B) + 1;
+	
+	if ((V-A) % (A-B) != 0) day++;
+	
+	cout << day;
+	
+	return 0;
+}
