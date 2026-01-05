b583add0b05f0b928a1a1302123680e0f31730b1
김희수 <khs.bunnybyee@gmail.com>
2026-01-05 15:42:18 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
 	int a, b;
-
+	
 	while(true){
 		cin >> a >> b;
-
-		// 종료 조건
-		if ((a == 0) && (b == 0)) break;
-
-		cout << a + b << '\n';
+		
+		if (a == 0 && b == 0) break;
+		
+		cout << a+b << '\n';
 	}
-}
+	
+	return 0;
+} 
