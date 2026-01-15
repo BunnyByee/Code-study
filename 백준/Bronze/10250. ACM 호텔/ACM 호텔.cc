a4f56d336d592872f3880cbc7d8f26ea000eed92
김희수 <khs.bunnybyee@gmail.com>
2026-01-15 16:24:48 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -5,17 +5,16 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	int T, H, W, N, result = 0;
+	int T, H, W, N;
 	cin >> T; // 테스트 개수 입력 
 	
-	for (int i=0; i<T; i++){
+	while (T--){
 		cin >> H >> W >> N; // 총 층 수, 방 수, 몇번째 손님
 		
-		// 나머지가 0이면 맨 꼭대기 층.
-		if (N % H == 0) result = H * 100 + N/H;
-		else result = (N%H) * 100 + N/H +1;
+		int floor = (N-1) % H + 1;
+		int room = (N-1) / H + 1;
 		
-		cout << result << '\n';
+		cout << floor * 100 + room << '\n';
 	}
 	
 	return 0;
