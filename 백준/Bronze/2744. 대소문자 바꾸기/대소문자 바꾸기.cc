987a2d7471a7595b3ccc47af384852089b65e8fb
김희수 <khs.bunnybyee@gmail.com>
2026-01-22 20:28:47 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cctype> // 타입 확인
+#include <string>
+using namespace std;
+
+int main(){
+	string str;
+	cin >> str;
+	
+	for(int i=0; i<str.size(); i++){
+		if (isupper(str[i]))
+			str[i] = tolower(str[i]);
+		else if (islower(str[i]))
+			str[i] = toupper(str[i]);			
+	}
+	
+	cout << str;
+	
+	return 0;
+}
