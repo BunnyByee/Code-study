91880d81654dbd7a42f207e02a660d2641e90e5f
김희수 <khs.bunnybyee@gmail.com>
2026-01-27 23:59:07 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-	int a, b;
+	long long a, b;
 	cin >> a >> b;
 	
-	cout << (a+b) * (a-b);
+	long long result = (a+b) * (a-b);
+	
+	cout << result <<"\n";
 	
 	return 0;
 }
